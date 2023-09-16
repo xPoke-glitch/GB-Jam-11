d790aaf44ce32ae371e08139a83322f2ace636c1
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-09-16 17:51:36 +0200

    Add - melee attack

--- Assets/Scripts/AI/MeleeAttack.cs
+++ Assets/Scripts/AI/MeleeAttack.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private AIData _aiData;
+
     public void Attack()
     {
+        _aiData.currentTarget.GetComponent<Player>().TakeDamage(1);
         Debug.Log("Melee");
     }
 }
