d6b4277bc9ccfb6ee0c457d3bb16a667f4a78e96
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-09-16 18:03:35 +0200

    Add - People rescue setup

---
+++ Assets/Scripts/Entities/Person/Person.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    private bool _rescueable = false;
+
+    private void Update()
+    {
+        if (!_rescueable)
+            return;
+
+        if(Input.GetKeyDown(KeyCode.L) || Input.GetKey("joystick button 0"))
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            _rescueable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            _rescueable = false;
+    }
+}
