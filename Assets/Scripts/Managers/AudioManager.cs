c9bb8bae3b692a9b3fbecc410a00f52c0b0b7b63
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-09-17 15:54:35 +0200

    Add - audiomanager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [Header("References")]
+    [SerializeField] List<AudioSource> _audioSources;
+
+    private AudioSource _bgMusicSource = null;
+
+    public void PlayGameBackgroundMusic(AudioClip audioClipToPlay, bool isLooped = false)
+    {
+        _bgMusicSource = FindFirstEmpty();
+        if (!_bgMusicSource)
+            return;
+
+        _bgMusicSource.clip = audioClipToPlay;
+        _bgMusicSource.loop = isLooped;
+        _bgMusicSource.Play();
+    }
+
+    public void StopGameBackgroundMusic()
+    {
+        _bgMusicSource.clip = null;
+        _bgMusicSource.loop = false;
+        _bgMusicSource.Stop();
+    }
+
+    public void PlayAudioEffect(AudioClip audioClipToPlay)
+    {
+        var audioSource = FindFirstEmpty();
+        if (!_bgMusicSource)
+            return;
+
+        audioSource.PlayOneShot(audioClipToPlay);
+    }
+
+    public void SetAudioMuted(bool isMuted)
+    {
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            audioSource.mute = isMuted;
+        }
+
+        if (_bgMusicSource)
+            _bgMusicSource.mute = isMuted;
+    }
+
+    private AudioSource FindFirstEmpty()
+    {
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            if (!audioSource.isPlaying)
+                return audioSource;
+        }
+
+        return null;
+    }
+}
