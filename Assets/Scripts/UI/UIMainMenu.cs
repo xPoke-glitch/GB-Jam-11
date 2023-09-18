d62365b27e95dc67b59f63c071dd0cba5fc33440
Cristian <cri.ghiorzi@gmail.com>
2023-09-18 20:02:34 +0200

    Basic MainMenu

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _startButton;
+    [SerializeField]
+    private TextMeshProUGUI _exitButton;
+
+    private float _time = 0.5f;
+    private Color colorA1;
+    private Color colorA0;
+    private void Start()
+    {
+        // Init anim
+        colorA0 = _startButton.color;
+        colorA0.a = 0;
+        _startButton.color = colorA0;
+        _exitButton.color = colorA0;
+        colorA1 = _startButton.color;
+        colorA1.a = 1;
+        FadePanel.Instance.FadeOut(() =>
+        {
+            LeanTween.value(this.gameObject, UpdateValueCallback, colorA0, colorA1, _time);
+        });
+    }
+
+    private void UpdateValueCallback(Color val)
+    {
+        _startButton.color = val;
+        _exitButton.color = val;
+    }
+
+    public void GoToGame()
+    {
+        FadePanel.Instance.FadeIn(() => {
+            SceneManager.LoadScene("GameScene");
+        });
+    }
+
+}
